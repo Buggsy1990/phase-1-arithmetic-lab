9b50e5c7757801c17d4d905e0a118a24bc3ca037
Christina <buggsy1106@gmail.com>
2022-03-02 17:44:48 -0600

    commit tree ddafc178ad0cf281c0b24cf30eca1f69c1104d5d

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+const num1 =31;
+const num2 =2;
+let multiply= num1*num2;
+let num3 = 16
+let num4 =4
+let mod = num3/num4
+const min =1
+const max = 20
+let random = Math.floor(Math.random()*(max-min+1) ) +min;
